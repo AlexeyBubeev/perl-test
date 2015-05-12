5635f93550d1f8390546a66baa2632693032aa35
AlexeyBubeev <alexey.bubeev@gmail.com>
2015-05-12 16:01:42 +0800

    fixed file

---
+++ lib/ABTest.pm
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+=pod
+Имена доменов и количество адресов в каждом домене. Сортировка по количеству адресов в домене, по убыванию. Отдельной строкой — количество не валидных адресов. Пример:
+     mail.ru     2
+     vk.com      1
+     rambler.ru  1
+     INVALID     1
+=cut
+
+package ABTest;
+
+use strict;
+use warnings;
+#use diagnostics;
+
+use Email::Valid; # все стандарты email адресов
+use Email::Address; # корректная работа с email
+use 5.18.2;
+
+open FILE, "../test.txt" or open FILE, "test.txt" or die "Can't open file";
+my (%emails,$bad_count,$adr_length,$max_length);
+
+$bad_count = 0;
+$max_length = 0;
+
+while (<FILE>) {
+    chomp; # отсекаем символ \n
+    if (Email::Valid->address($_)){
+        my ($addr) = Email::Address->parse($_);
+        $emails{$addr->host}++;
+        my $adr_length = length($addr->host);
+        if($max_length < $adr_length){
+            $max_length = $adr_length;
+        }
+    } else {
+        $bad_count++;
+    }
+}
+close FILE;
+
+$max_length = ($max_length)*-1;
+
+foreach my $str (sort keys %emails) {
+    printf "%${max_length}s %s\n", $str, $emails{$str};
+}
+
+print "INVALID:".$bad_count;
+
+
+
+1;
--- t/ABTest.t
+++ t/ABTest.t
@@ -1,23 +1,24 @@
 use strict;
 use warnings;
-use lib ".";
 use Test::More;
 use ABTest;
 
 BEGIN{
     use_ok( 'Email::Valid' ) || print "Email::Valid not found\n";
     use_ok( 'Email::Address' ) || print "Email::Address not found \n";
+    use_ok( 'ABTest' ) || print "ABTest not found \n";
     require_ok( 'Email::Valid' );
     require_ok( 'Email::Address' );
-}
+    require_ok( 'ABTest' );
+};
 
 subtest 'check test.txt file' => sub {
-    my $file='./test.txt';
-    ok(checkfile($file)==1,'File found');
+    my $file='test.txt';
     sub checkfile{
         if (-e $_[0]) { return 1;}
             else { return 0; }
     }
+    ok(checkfile($file)==1,'File found');
 };
 
 subtest 'test email' => sub {
@@ -27,10 +28,9 @@ subtest 'test email' => sub {
 };
 
 subtest 'test email domain path' => sub {
-    my $test_email = 'username@domenname.com';
-    my $el = length(Email::Address->parse($test_email));
-    ok($el == 12,'test email domian path');
+    my $test_email_d = 'username@domenname.com';
+    my $el_d = Email::Address->parse($test_email_d);
+    ok($el_d==1,'test email domian path');
 };
 
-
-done_testing;
+done_testing();
